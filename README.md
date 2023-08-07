4660ab5be0b93006bb410c2409eb014f8d155339
Ananya <patil08ananya@gmail.com>
2023-08-08 01:18:39 +0530

    index.css edited

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Created using Node.js, Express.js and EJS
+
+color pallete:
+#03045e
+#0077b6
+#00b4d8
+#90e0ef
+#caf0f8
+
+* for image check in public/images folder *
---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server started at ${port}`);
+});
