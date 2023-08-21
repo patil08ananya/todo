2dd30c17a61e7f3f7813a0e63978dae1cb2425ee
Ananya <patil08ananya@gmail.com>
2023-08-21 23:34:13 +0530

    list items added ( final commit 1)

--- index.js
+++ index.js
@@ -9,18 +9,22 @@ const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 let today = date.getDate();
 let mon = date.getMonth();
 let year = date.getFullYear();
 
+let newTasks = [];
+
 app.get("/", (req, res) => {
-    res.render("index.ejs",{currentDate: today, currentMonth: month[mon], currentYear: year});
+    res.render("index.ejs",{currentDate: today, currentMonth: month[mon], currentYear: year, newTaskOfDay: newTasks});
 });
 
 app.post("/submit", (req, res) => {
     let newTask = req.body["taskForToday"];
-    res.render("index.ejs",{currentDate: today, currentMonth: month[mon], currentYear: year,task: newTask});
+    newTasks.push(newTask);
+    res.redirect("/");
 });
 
 
